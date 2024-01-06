93237a747ffda68f7760d4600c4a04c911311f4f
Josh Philpott <jphil529@gmail.com>
2024-01-05 22:53:11 -0700

    add eleventy base plugin

--- client/.eleventy.js
+++ client/.eleventy.js
@@ -1,10 +1,12 @@
 const purge = require("./purge");
+const { EleventyHtmlBasePlugin } = require("@11ty/eleventy");
 
 module.exports = function (eleventyConfig, options = {}) {
   // Copy `img/` to `_site/img`
   eleventyConfig.addPassthroughCopy("docs");
 
   eleventyConfig.on("afterBuild", () => purge(options));
+  eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
 
   return {
     dir: {
